3d7bfbb297a84474988a9191761e14951235858a
Souradeep Roy <souro26.ju@gmail.com>
2026-06-06 16:37:57 +0530

    added init

---
+++ tokenizer/__init__.py
@@ -0,0 +1,10 @@
+"""
+tokenizer/
+
+BPE tokenizer trained on Python code.
+Import SLMTokenizer from here for all tokenization needs.
+"""
+
+from tokenizer.tokenizer import SLMTokenizer
+
+__all__ = ["SLMTokenizer"]
